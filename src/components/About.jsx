87e6b019c0e39fae6dc06031820abc48da487612
suryaenders <suryanallasanthala666@gmail.com>
2026-05-25 15:15:27 +0530

    Footer Component added

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -20,7 +20,7 @@ const About = () => {
       <div className="flex flex-col md:flex-row items-center md:items-start md:gap-20">
         <img src={assets.brand_img} className="w-full sm:w-1/2 max-w-lg" />
         <div className="flex flex-col items-center md:items-start mt-10 text-gray-600">
-          <div className="grid grid-cols-2 gap-6 md:gap-10 w-full 2xl pr-28">
+          <div className="grid grid-cols-2 gap-6 md:gap-10 w-full 2xl:pr-28">
             <div>
               <p className="text-4xl font-medium text-gray-800">10+</p>
               <p>Years of Experience</p>
