aac68e3555053359fbbdf0adbbae35fadc4f3ea3
suryaenders <suryanallasanthala666@gmail.com>
2026-05-25 15:43:04 +0530

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 // import React from 'react'
 import About from "./components/About";
-import { Footer } from "./components/Footer";
+import Footer from "./components/Footer";
 import Header from "./components/Header";
 import Projects from "./components/Projects";
 import Testimonials from "./components/Testimonials";
@@ -11,13 +11,13 @@ const App = () => {
   return (
     <>
       <div className="w-full overflow-hidden">
-        <ToastContainer />
-        <Header />
         <About />
+        <Footer />
+        <Header />
         <Projects />
         <Testimonials />
         <Contact />
-        <Footer />
+        <ToastContainer />
       </div>
     </>
   );
