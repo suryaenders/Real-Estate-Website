19a6dee28b9d61bb981dbf85d78c4f2be8f87e52
suryaenders <suryanallasanthala666@gmail.com>
2026-05-25 13:54:39 +0530

    Contact page is created

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 // import React from 'react'
 import About from "./components/About";
-// import Form from "./components/Form";
 import Header from "./components/Header";
 import Projects from "./components/Projects";
 import Testimonials from "./components/Testimonials";
+import Contact from "./pages/Contact";
 
 const App = () => {
   return (
@@ -13,6 +13,7 @@ const App = () => {
         <About />
         <Projects />
         <Testimonials />
+        <Contact />
       </div>
     </>
   );
